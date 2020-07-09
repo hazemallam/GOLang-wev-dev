dc4bccd8cea0a277485ba333f7674475d1fa142b
Hazem Allam <s.enghazemallm@gmail.com>
2020-07-09 12:15:42 +0200

    first commit

---
+++ 06_funcs/02_date_formatting/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"text/template"
+	"os"
+	"log"
+	"time"
+)
+
+var tpl *template.Template
+
+var fm = template.FuncMap{
+	"tf" : TimeFormating,
+}
+
+func TimeFormating(t time.Time) string {
+	return t.Format("02-01-2006")
+}
+
+func init(){
+	tpl = template.Must(template.New("").Funcs(fm).ParseFiles("main.gohtml"))
+}
+
+func main(){
+
+	err := tpl.ExecuteTemplate(os.Stdout, "main.gohtml", time.Now())
+	if err != nil{
+		log.Fatalln("error occured", err)
+	}
+}
