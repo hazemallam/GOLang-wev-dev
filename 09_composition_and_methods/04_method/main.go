3a6df22e602e6fd9ac54f38421bf1c8b9c3aeb46
Hazem Allam <s.enghazemallm@gmail.com>
2020-07-10 14:00:57 +0200

    first commit

---
+++ 09_composition_and_methods/04_method/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"text/template"
+	"os"
+	"log"
+)
+
+type person struct{
+	Name string
+	Age int
+}
+
+func (p person) SomeProcessing() int{
+	return 7
+}
+
+func (p person) AgeDbl() int{
+	return p.Age * 2
+}
+
+func (p person) TakesArg(x int) int {
+	return x * 2
+} 
+
+var tpl *template.Template
+
+func init(){
+	tpl = template.Must(template.ParseFiles("main.gohtml"))
+}
+
+func main(){
+
+	p := person{
+		Name : "James",
+		Age : 42,
+	}
+
+	err := tpl.Execute(os.Stdout, p)
+	if err != nil{
+		log.Fatalln("error occured", err)
+	}
+}
