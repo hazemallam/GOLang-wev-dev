22d79a8a3ab10e128561f25daa76151e1ef4f9dc
Hazem Allam <s.enghazemallm@gmail.com>
2020-07-10 12:09:40 +0200

    first commit

---
+++ 09_composition_and_methods/01/main.go
@@ -0,0 +1,30 @@
+package main
+
+import(
+	"text/template"
+	"os"
+	"log"
+)
+
+var tpl *template.Template
+
+type person struct{
+	Name string
+	Age int
+}
+
+func init(){
+	tpl = template.Must(template.ParseFiles("main.gohtml"))
+}
+
+
+func main(){
+	p1 := person{
+		Name : "James Bond",
+		Age : 42,
+	}
+	err := tpl.Execute(os.Stdout, p1)
+	if err != nil{
+		log.Fatalln("error occured", err)
+	}
+}
