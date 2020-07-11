b6d1b05eb4cba10fe1acdc87ea1be4754e0d6f88
Hazem Allam <s.enghazemallm@gmail.com>
2020-07-11 12:37:53 +0200

    first commit

---
+++ 03_understanding_servers/01_understanding_TCP_server/03_read_write/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":5050")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer li.Close()
+
+	for {
+		con, err := li.Accept()
+
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go handle(con)
+	}
+}
+
+func handle(con net.Conn) {
+	scanner := bufio.NewScanner(con)
+
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		fmt.Fprintf(con, "I heard you say: %s\n", ln)
+	}
+
+	// we never get here
+	// we have an open stream connection
+	// how does the above reader know when it's done?
+	fmt.Println("Code got here.")
+}
