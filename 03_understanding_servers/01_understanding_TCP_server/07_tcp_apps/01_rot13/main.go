0a77422042ee59ec05260fd1a5b885010e7510bf
Hazem Allam <s.enghazemallm@gmail.com>
2020-07-11 14:38:41 +0200

    first message

---
+++ 03_understanding_servers/01_understanding_TCP_server/07_tcp_apps/01_rot13/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":5050")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer li.Close()
+	for {
+		con, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go handle(con)
+	}
+}
+
+func handle(con net.Conn) {
+	scanner := bufio.NewScanner(con)
+	for scanner.Scan() {
+		ln := strings.ToLower(scanner.Text())
+		bs := []byte(ln)
+		r := rot13(bs)
+		fmt.Fprintf(con, "%s - %s\n\n", ln, r)
+	}
+}
+
+func rot13(bs []byte) []byte {
+	var r13 = make([]byte, len(bs))
+	for i, v := range bs {
+		if v <= 109 {
+			r13[i] = v + 13
+		} else {
+			r13[i] = v - 13
+		}
+	}
+	return r13
+}
