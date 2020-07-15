89b47a0a477b053f54de7b38c7a918341e89b13a
Hazem Allam <s.enghazemallm@gmail.com>
2020-07-15 16:50:48 +0200

    first commit

---
+++ 06_serving_files/02_serving/04_FileServer/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("./images")))
+	http.HandleFunc("/dog/", dog)
+	http.ListenAndServe(":5050", nil)
+}
+
+func dog(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(res, `<img src="/gold.jpg">`)
+}
