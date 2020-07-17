dce549dcd5ebe7801b3207cf505525b040dfdb9d
Hazem Allam <s.enghazemallm@gmail.com>
2020-07-17 18:58:19 +0200

    first commit

---
+++ 07_creating_state/01_passing_data/02_form_post/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":5050", nil)
+
+}
+
+func foo(res http.ResponseWriter, req *http.Request) {
+	value := req.FormValue("name")
+	res.Header().Set("content-type", "text/html; charset=utf-8")
+	io.WriteString(res, `
+		<!DOCTYPE html>
+			<head>
+				<meta charset="UTF-8">
+				<title>state</title>
+			</head>
+			<body>
+				<form method="POST">
+					<input type="text" name="name">
+					<input type="submit">
+				</form>
+			</body>
+		</html>
+	`+value)
+}
