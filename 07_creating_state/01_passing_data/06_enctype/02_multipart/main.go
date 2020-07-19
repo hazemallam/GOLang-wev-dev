40fd5ccf8d991bfcea81fb545c01e234eed31d4b
Hazem Allam <s.enghazemallm@gmail.com>
2020-07-19 19:12:24 +0200

    first commit

---
+++ 07_creating_state/01_passing_data/06_enctype/02_multipart/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"log"
+)
+
+var tpl *template.Template
+
+type user struct{
+	First , Last string
+	Sub bool
+}
+
+func init(){
+	tpl = template.Must(template.ParseGlob("templates/*.html"))
+}
+
+func main(){
+	http.HandleFunc("/", serve)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	log.Fatalln(http.ListenAndServe(":5050", nil))
+
+}
+
+func serve(res http.ResponseWriter, req *http.Request){
+	//body
+	bs := make([]byte, req.ContentLength)
+	req.Body.Read(bs)
+	body := string(bs)
+	err := tpl.ExecuteTemplate(res, "index.html", body)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		log.Fatalln(err)
+	}
+}
