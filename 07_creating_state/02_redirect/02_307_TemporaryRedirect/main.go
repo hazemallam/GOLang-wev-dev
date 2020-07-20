15f0b0cfa532b81522804c05d7c13df16e98cb30
Hazem Allam <s.enghazemallm@gmail.com>
2020-07-20 14:08:13 +0200

    first commit

---
+++ 07_creating_state/02_redirect/02_307_TemporaryRedirect/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"html/template"
+)
+
+var tpl *template.Template
+
+func init(){
+	tpl = template.Must(template.ParseGlob("templates/*.html"))
+}
+
+func main(){
+	http.HandleFunc("/", foo)
+	http.HandleFunc("/bar", bar)
+	http.HandleFunc("/barred", barred)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":5050", nil)
+}
+
+func foo(res http.ResponseWriter, req *http.Request){
+	fmt.Print("your request method at foo: ",req.Method,"\n\n")
+}
+
+func bar(res http.ResponseWriter, req *http.Request){
+	fmt.Print("your request method at bar: ", req.Method, "\n\n")
+	//process from submission
+	http.Redirect(res, req, "/", http.StatusTemporaryRedirect)
+}
+
+func barred(res http.ResponseWriter, req *http.Request){
+	fmt.Print("your request method at barred: ", req.Method, "\n\n")
+	tpl.ExecuteTemplate(res, "index.html", nil)
+}
